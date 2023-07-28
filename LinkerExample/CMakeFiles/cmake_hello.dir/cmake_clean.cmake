ea3d0c7759cb379746eb54c731ed9e88b2520cb2
wilfredkisku <wilfred.kisku@gmail.com>
2023-07-28 05:30:51 +0530

    Linker Example

---
+++ LinkerExample/CMakeFiles/cmake_hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_hello.dir/src/main.cpp.o"
+  "cmake_hello.pdb"
+  "cmake_hello"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
