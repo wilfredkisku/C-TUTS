881331a94e1b48abd58053c79a990cee8f35a72d
wilfredkisku <wilfred.kisku@gmail.com>
2023-07-31 17:50:29 +0530

    memoization

--- Fibonacci/fib.py
+++ Fibonacci/fib.py
@@ -1,13 +1,17 @@
+import time
 
-
-def fib(n):
-
+def fib(n, memo={}):
+    if (n in memo):
+        return memo[n]
     if n<=2:
         return 1
 
-    return fib(n-1) + fib(n-2)
+    memo[n] = fib(n-1) + fib(n-2)
+    return memo[n]
 
 print(fib(7))
 print(fib(15))
-print(fib(50))
+start_time = time.time()
+print(fib(100))
+print("--- %s seconds ---" % (time.time() - start_time))
 
